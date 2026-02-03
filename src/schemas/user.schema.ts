cbe23df8fcbeb015fdb100de47d1ba2bc78adb65
elouannasse <elouannassez@gmail.com>
2026-02-03 11:14:16 +0100

    RD-372 Create User entity (DB)

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { UserRole } from '../enums/user-role.enum';
+
+export type UserDocument = User & Document;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ 
+    type: String, 
+    enum: UserRole, 
+    default: UserRole.PARTICIPANT 
+  })
+  role: UserRole;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
